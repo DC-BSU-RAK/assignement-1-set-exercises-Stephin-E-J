fb94ac83ff939d5b911f914a2055b33801f85cfa
Captaino <stephinjohnson06@gmail.com>
2025-04-21 18:50:41 +0400

    Chapter-6

--- Chapter-6-Javascript/Exercises/Chapter-6 javascript.js
+++ Chapter-6-Javascript/Exercises/Chapter-6 javascript.js
@@ -1,8 +1,9 @@
 function calculateTotal() { /*The function calculateTotal() is used for calculating the values */
     /**The const tag also known as constant is used to prevent other users from changing it */
     const pricePerLiter = parseFloat(document.getElementById('price').value);
+    /**parseFloat is to convert string to a floating-point*/
     const litersPurchased = parseFloat(document.getElementById('liter').value);
-    
+    /**The * is used for multiplication of two values*/
     const totalCost = pricePerLiter * litersPurchased;
     
     document.getElementById('totalCost').textContent = 
